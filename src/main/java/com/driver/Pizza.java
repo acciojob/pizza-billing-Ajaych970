c7c2d3bdecae9c0e506bf581af8a1c8ea1d7886d
Mrfantasticc <ajay.electromania@gmail.com>
2023-08-14 18:59:01 +0530

    Attempt 3

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -8,12 +8,13 @@ public class Pizza {
 
     private int basePrice;
     private int extraCheesePrice;
-    private int extraToppingsPrice;
+    private int extraToppingsPriceNonVeg;
     private int paperbagPrice;
     private  boolean isExtraCheeseAdded;
     private boolean isExtraToppingsAdded;
     private boolean isPaperBagAdded;
     private boolean isBillGenerated;
+    private int extraToppingPriceVeg;
 
 
     public Pizza(Boolean isVeg){
@@ -21,13 +22,15 @@ public class Pizza {
         // your code goes here
         if(this.isVeg==true){
             this.price=300;
-            this.extraToppingsPrice=70;
+            //this.extraToppingPriceVeg=70;
             this.basePrice=300;
         }else {
             this.basePrice=400;
-            this.extraToppingsPrice=120;
+           // this.extraToppingsPriceNonVeg=120;
             this.price=400;
         }
+        this.extraToppingsPriceNonVeg=120;
+        this.extraToppingPriceVeg=70;
         this.extraCheesePrice=80;
         this.paperbagPrice=20;
         //this.price=this.basePrice;
@@ -50,8 +53,11 @@ public class Pizza {
 
     public void addExtraToppings(){
         // your code goes here
-        if(this.isExtraToppingsAdded==false){
-            this.price=this.price+this.extraToppingsPrice;
+        if(this.isExtraToppingsAdded==false && isVeg==true ){
+            this.price=this.price+this.extraToppingPriceVeg;
+            this.isExtraToppingsAdded=true;
+        } else if (this.isExtraToppingsAdded==false && isVeg==false) {
+            this.price=this.price+this.extraToppingsPriceNonVeg;
             this.isExtraToppingsAdded=true;
         }
     }
@@ -66,20 +72,23 @@ public class Pizza {
 
     public String getBill(){
         // your code goes here
-        if(this.isBillGenerated==false){
+        //if(this.isBillGenerated==false){
             this.bill="Base Price Of The Pizza: "+this.basePrice+"\n";
             if(this.isExtraCheeseAdded==true){
                 this.bill+="Extra Cheese Added: "+this.extraCheesePrice +"\n";
             }
-            if(this.isExtraToppingsAdded==true){
-                this.bill += "Extra Toppings Added: "+this.extraToppingsPrice+"\n";
+            if(this.isExtraToppingsAdded==true && isVeg==true){
+                this.bill += "Extra Toppings Added: "+this.extraToppingPriceVeg+"\n";
+            }
+            else if(this.isExtraToppingsAdded==true && isVeg==false){
+                this.bill += "Extra Toppings Added: "+this.extraToppingsPriceNonVeg+"\n";
             }
             if(this.isPaperBagAdded==true){
                 this.bill +="Paperbag Added: "+this.paperbagPrice+"\n";
             }
-            this.bill +="Total Price: "+this.price;
-            this.isBillGenerated=true;
-        }
+            this.bill +="Total Price: "+this.price+"\n";
+            //this.isBillGenerated=true;
+       // }
         return this.bill;
 
     }
