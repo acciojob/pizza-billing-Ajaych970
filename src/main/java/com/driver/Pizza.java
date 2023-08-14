f7509b8c7c657fd21c7f67098e738464757575e0
Mrfantasticc <ajay.electromania@gmail.com>
2023-08-14 19:12:21 +0530

    Attempt 5

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -8,32 +8,29 @@ public class Pizza {
 
     private int basePrice;
     private int extraCheesePrice;
-    private int extraToppingsPriceNonVeg;
+    private int extraToppingsPrice;
     private int paperbagPrice;
     private  boolean isExtraCheeseAdded;
     private boolean isExtraToppingsAdded;
     private boolean isPaperBagAdded;
     private boolean isBillGenerated;
-    private int extraToppingPriceVeg;
 
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
         if(this.isVeg==true){
-            this.price=300;
-            //this.extraToppingPriceVeg=70;
+            //this.price=300;
+            this.extraToppingsPrice=70;
             this.basePrice=300;
         }else {
             this.basePrice=400;
-           // this.extraToppingsPriceNonVeg=120;
-            this.price=400;
+            this.extraToppingsPrice=120;
+            //this.price=400;
         }
-        this.extraToppingsPriceNonVeg=120;
-        this.extraToppingPriceVeg=70;
         this.extraCheesePrice=80;
         this.paperbagPrice=20;
-        //this.price=this.basePrice;
+        this.price=this.basePrice;
 
     }
 
@@ -53,11 +50,8 @@ public class Pizza {
 
     public void addExtraToppings(){
         // your code goes here
-        if(this.isExtraToppingsAdded==false && isVeg==true ){
-            this.price=this.price+this.extraToppingPriceVeg;
-            this.isExtraToppingsAdded=true;
-        } else if (this.isExtraToppingsAdded==false && isVeg==false) {
-            this.price=this.price+this.extraToppingsPriceNonVeg;
+        if(this.isExtraToppingsAdded==false){
+            this.price=this.price+this.extraToppingsPrice;
             this.isExtraToppingsAdded=true;
         }
     }
@@ -72,23 +66,20 @@ public class Pizza {
 
     public String getBill(){
         // your code goes here
-        //if(this.isBillGenerated==false){
+        if(this.isBillGenerated==false){
             this.bill="Base Price Of The Pizza: "+this.basePrice+"\n";
             if(this.isExtraCheeseAdded==true){
                 this.bill+="Extra Cheese Added: "+this.extraCheesePrice +"\n";
             }
-            if(this.isExtraToppingsAdded==true && isVeg==true){
-                this.bill += "Extra Toppings Added: "+this.extraToppingPriceVeg+"\n";
-            }
-            else if(this.isExtraToppingsAdded==true && isVeg==false){
-                this.bill += "Extra Toppings Added: "+this.extraToppingsPriceNonVeg+"\n";
+            if(this.isExtraToppingsAdded==true){
+                this.bill += "Extra Toppings Added: "+this.extraToppingsPrice+"\n";
             }
             if(this.isPaperBagAdded==true){
                 this.bill +="Paperbag Added: "+this.paperbagPrice+"\n";
             }
             this.bill +="Total Price: "+this.price+"\n";
-            //this.isBillGenerated=true;
-       // }
+            this.isBillGenerated=true;
+       }
         return this.bill;
 
     }
